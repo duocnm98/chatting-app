4c389dbea6fa81a01ede514ff140d57bf132fdd0
kiviengdpt <kiviengdpt@gmail.com>
2019-11-27 08:43:08 +0700

    logout-function

--- src/routes/web.route.js
+++ src/routes/web.route.js
@@ -10,21 +10,23 @@ initPassportLocal();
 
 let router = express.Router();
 
-router.get("/", home.index);
+router.get("/login-register", auth.checkLoggedOut, auth.loginRegister);
 
-router.get("/login-register", auth.loginRegister);
+router.get('/test-database', auth.checkLoggedOut, home.testDatabase);
 
-router.get('/test-database', home.testDatabase);
+router.post('/register', auth.checkLoggedOut, authValid.register, auth.postRegister);
 
-router.post('/register', authValid.register, auth.postRegister);
+router.get('/verify/:token', auth.checkLoggedOut, auth.verifyAccount);
 
-router.get('/verify/:token', auth.verifyAccount);
-
-router.post('/login', passport.authenticate('local', {
+router.post('/login', auth.checkLoggedOut, passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login-register',
   successFlash: true,
   failureFlash: true
 }));
+
+
+router.get("/", auth.checkLoggedIn ,home.index);
+router.get('/logout',auth.checkLoggedIn, auth.getLogout);
 
 module.exports = router;
