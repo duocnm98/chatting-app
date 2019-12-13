f72cff9f2bb4c672b73b5bff333f13ab59a98ba6
kiviengdpt <kiviengdpt@gmail.com>
2019-12-13 11:43:58 +0700

    add-remove-contact-realtime

--- src/public/js/addContact.js
+++ src/public/js/addContact.js
@@ -1,15 +1,33 @@
 function addContact() {
   $(".user-add-new-contact").bind("click", function() {
     let targetId = $(this).data("uid");
-    $.post("/contact/add-new", {uid: targetId}, function(data) {
-      if(data.success) {
-        $("#find-user").find(`div.user-add-new-contact[data-uid = ${targetId}]`).hide();
-        $("#find-user").find(`div.user-remove-request-contact[data-uid = ${targetId}]`).css("display", "inline-block");
+    $.post("/contact/add-new", { uid: targetId }, function(data) {
+      if (data.success) {
+        $("#find-user")
+          .find(`div.user-add-new-contact[data-uid = ${targetId}]`)
+          .hide();
+        $("#find-user")
+          .find(`div.user-remove-request-contact[data-uid = ${targetId}]`)
+          .css("display", "inline-block");
         increaseNumberNotifContact("count-request-contact-sent");
 
         //real-time processing
-        socket.emit("add-new-contact",{contactId: targetId});
+        socket.emit("add-new-contact", { contactId: targetId });
       }
     });
   });
-} 
+}
+
+//listen socket event from server
+socket.on("response-add-new-contact", user => {
+  let notif = `<span data-uid="${user.id}">
+                <img class="avatar-small" src="images/users/${user.avatar}" alt=""> 
+                <strong>${user.username}</strong> đã gửi cho bạn lời mời kết bạn!
+              </span><br><br><br>`;
+  $(".noti_content").prepend(notif);
+
+  increaseNumberNotifContact("count-request-contact-received");
+
+  increaseNumberNotification("noti_contact_counter");
+  increaseNumberNotification("noti_counter");
+});
--- src/public/js/removeRequestContact.js
+++ src/public/js/removeRequestContact.js
@@ -10,8 +10,22 @@ function removeRequestContact() {
           $("#find-user").find(`div.user-remove-request-contact[data-uid = ${targetId}]`).hide();
           $("#find-user").find(`div.user-add-new-contact[data-uid = ${targetId}]`).css("display", "inline-block");
           decreaseNumberNotifContact("count-request-contact-sent");
+          //real-time processing
+          socket.emit("remove-request-contact", { contactId: targetId });
         }
       }
     });
   });
 }
+
+//listen socket io remove request contact event
+socket.on("response-remove-request-contact", user => {
+  $(".noti_content").find(`span[data-uid = ${user.id}]`).remove();
+
+  //Delete at tab model add friend request
+  decreaseNumberNotifContact("count-request-contact-received");
+
+  decreaseNumberNotification("noti_contact_counter");
+  decreaseNumberNotification("noti_counter");
+});
+
